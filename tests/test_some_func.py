aa7bc3662c76e165069a9dfe2a8213958a6c24f9
Andrey_Yudakov <viewsoul91@gmail.com>
2023-05-30 18:16:43 +0300

    init commit

---
+++ tests/test_some_func.py
@@ -0,0 +1,10 @@
+from some_func import capitalize
+
+
+if capitalize('hello') != 'Hello':
+    raise Exception('Функция работает неверно!')
+
+if capitalize('') != '':
+    raise Exception('Функция работает неверно!')
+
+print('Все тесты пройдены!')
